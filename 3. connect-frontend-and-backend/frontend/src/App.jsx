bf01985bf35bcf5cb5d6ec7d0e98220949b4e43c
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-04 12:52:16 +0500

    api call with axiox

--- 3. connect-frontend-and-backend/frontend/src/App.jsx
+++ 3. connect-frontend-and-backend/frontend/src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/jokes")
+      .then((response) => {
+        setJokes(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching jokes", error);
+      });
+  }, []);
 
   return (
     <>
       <h1>Full Stack Hello world</h1>
+      <p>Jokes: {jokes.length}</p>
+
+      {jokes.map((joke, idx) => (
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      ))}
     </>
   );
 }
