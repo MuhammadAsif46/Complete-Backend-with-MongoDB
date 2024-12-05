e7da34b23db9ada5288c59d835c583af6272618a
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-05 18:23:06 +0500

    complete todo schema

--- 4. dataModeling-with-mongoose/models/todos/todo.models.js
+++ 4. dataModeling-with-mongoose/models/todos/todo.models.js
@@ -1,8 +1,28 @@
 import mongoose from "mongoose";
 
-const todoSchema = new mongoose.Schema({
+const todoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    // this fields is define to which schema connect to another schema
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    subTodos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SubTodo",
+      },
+    ], // Array of sub-todos
+  },
+  { timestamps: true }
+);
 
-}, { timestamps: true });
-
-
-export const todo = mongoose.model("Todo", todoSchema);
+export const Todo = mongoose.model("Todo", todoSchema);
