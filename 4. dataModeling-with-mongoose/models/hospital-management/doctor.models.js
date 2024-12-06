1e1c4bc8a724f79ec3f78641e6a9d3d2b091352f
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-06 16:25:29 +0500

    add doctor schema

--- 4. dataModeling-with-mongoose/models/hospital-management/doctor.models.js
+++ 4. dataModeling-with-mongoose/models/hospital-management/doctor.models.js
@@ -1,5 +1,36 @@
 import mongoose from "mongoose";
 
-const doctorSchema = new mongoose.Schema({}, { timestamps: true });
+const doctorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    specialization: {
+      type: String,
+      required: true,
+    },
+    experienceInYear: {
+      type: Number,
+      default: 0,
+    },
+    salary: {
+      type: String,
+      required: true,
+    },
+    qualification:{
+        type: String,
+        required: true,
+    },
+    worksInHospitals:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Hospital",
+        },
+        
+    ]
+  },
+  { timestamps: true }
+);
 
 export const Doctor = mongoose.model("Doctor", doctorSchema);
