3d468dd5ea37c7d5637b7dca3596a54582cac70d
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-06 16:35:29 +0500

    add hospital schema

--- 4. dataModeling-with-mongoose/models/hospital-management/hospital.models.js
+++ 4. dataModeling-with-mongoose/models/hospital-management/hospital.models.js
@@ -1,5 +1,38 @@
 import mongoose from "mongoose";
 
-const hospitalSchema = new mongoose.Schema({}, { timestamps: true });
+const hospitalSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    addressLine1: {
+      type: String,
+      required: true,
+    },
+    addressLine2: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    pincode: {
+      type: String,
+      required: true,
+    },
+    specialized: [
+      {
+        type: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 export const Hospital = mongoose.model("Hospital", hospitalSchema);
