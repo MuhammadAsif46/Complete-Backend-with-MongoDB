ff5a350a9e3baa7603829e21788933e7bbc54f95
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-06 15:31:05 +0500

    add product scheme

--- 4. dataModeling-with-mongoose/models/ecommerce/order.models.js
+++ 4. dataModeling-with-mongoose/models/ecommerce/order.models.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-const orderSchema = new mongoose.Schema({}, { timestamps: true });
+const orderSchema = new mongoose.Schema({},{ timestamps: true });
 
 export const Order = mongoose.model("Order", orderSchema);
--- 4. dataModeling-with-mongoose/models/ecommerce/product.models.js
+++ 4. dataModeling-with-mongoose/models/ecommerce/product.models.js
@@ -1,5 +1,38 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({}, { timestamps: true });
+const productSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    productImage: {
+      // add image in cloudnary and return cloudnary string url then save it
+      type: String,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
 export const Product = mongoose.model("Product", productSchema);
