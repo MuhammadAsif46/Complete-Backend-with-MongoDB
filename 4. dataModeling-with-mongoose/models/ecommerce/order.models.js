d439a4d0b7b023f85606e081033864022f7cb563
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-06 15:59:11 +0500

    add Order schema

--- 4. dataModeling-with-mongoose/models/ecommerce/order.models.js
+++ 4. dataModeling-with-mongoose/models/ecommerce/order.models.js
@@ -1,5 +1,54 @@
 import mongoose from "mongoose";
 
-const orderSchema = new mongoose.Schema({},{ timestamps: true });
+const orderItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    orderPrice: {
+      type: Number,
+      required: true,
+    },
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    orderItems: {
+      //  choose any one both are same working
+      type: [orderItemSchema],
+      //   type: [
+      //     {
+      //       productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+      //       quantity: { type: Number },
+      //     },
+      //   ],
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      enum: ["PayPal", "Credit Card", "Apple Pay"],
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["PENDING", "DELIVERED", "CANCALLED"],
+      default: "PENDING",
+    },
+  },
+  { timestamps: true }
+);
 
 export const Order = mongoose.model("Order", orderSchema);
